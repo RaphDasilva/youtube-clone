b982f4ee35564b7652f3c24fe95b80bb695a5ec4
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-09-13 17:10:31 +0100

    Implement onClick function on the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,7 @@
 import { Stack } from "@mui/material";
 import { categories } from "../utils/constants";
 
-const SelectedCategory = "New";
-
-
-const Sidebar = () => 
+const Sidebar = ({selectedCategory,setSelectedCategory }) => 
   (
     <Stack 
     direction="row"
@@ -15,11 +12,12 @@ const Sidebar = () =>
         }}>
         {categories.map((category) => (
             <button className="category-btn"
-            style={{background: category.name === SelectedCategory && "#FC1503", color: '#fff'}}
+            onClick={() => setSelectedCategory(category.name)}
+            style={{background: category.name === selectedCategory && "#FC1503", color: '#fff'}}
             key={category.name}
             >
-                <span style={{color: category.name === SelectedCategory ? "#fff" : "red", marginRight: '15px'}}>{category.icon}</span>
-                <span style={{opacity: category.name === SelectedCategory ? "1" : "0.8"}}>{category.name}</span>
+                <span style={{color: category.name === selectedCategory ? "#fff" : "red", marginRight: '15px'}}>{category.icon}</span>
+                <span style={{opacity: category.name === selectedCategory ? "1" : "0.8"}}>{category.name}</span>
             </button>
         ))}
     </Stack>
